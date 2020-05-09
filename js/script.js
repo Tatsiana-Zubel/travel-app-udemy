6476dfabecb3479c65b0745d5fe77a24fc021f12
Tatsiana Zubel <>
2020-05-09 19:00:11 +0300

    added functionality to send POST request to the server

--- js/script.js
+++ js/script.js
@@ -35,7 +35,6 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
     //Timer
     let deadline = '2021-05-02';
 
@@ -67,6 +66,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 return param;
             };
         }
+
         function updateClock() {
             let t = getTimeRemaining(endtime);
             hours.textContent = addZero(t.hours);
@@ -83,4 +83,80 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('timer', deadline);
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        descriptionBtn = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    descriptionBtn.forEach(tab => {
+        tab.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = 'visible';
+    });
+
+    // Form
+
+    let message = {
+
+        loading: 'Загрузка',
+        success: 'Спасибо! Мы скоро свяжемся с вами!',
+        failure: 'Что-то пошло не так...'
+    }
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+        
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readuState === 4 && request.status === 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = ''; 
+        }
+    });
+    
 });
