fdfb7ad891cbf295252fdf04089387fd5fca9ec4
Tatsiana Zubel <>
2020-05-16 18:27:35 +0300

    Use Promise

--- js/script.js
+++ js/script.js
@@ -116,47 +116,61 @@ window.addEventListener('DOMContentLoaded', function () {
     // Form
 
     let message = {
-
         loading: 'Загрузка',
         success: 'Спасибо! Мы скоро свяжемся с вами!',
         failure: 'Что-то пошло не так...'
     }
     let form = document.querySelector('.main-form'),
-        input = form.getElementsByTagName('input'),
+        formBottom = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
     statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(e) {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+            function postData(data) {
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-        let formData = new FormData(form);
-        
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 300) {
+                                resolve()
+                            } else {
+                                reject()
+                            }
+                        }
+                    }
+                    request.send(data);
+                })
+            } //End postData 
 
-        request.send(json);
-
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readuState === 4 && request.status === 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
             }
+
+            postData(formData)
+                .then(() => StatusMessage.innerHTML = message.loading)
+                .then(() => {
+                    thankModal.style.display = 'block';
+                    mainModal.style.display = 'none';
+                    statusMessage.innerHTML = '';
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
         });
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = ''; 
-        }
-    });
-    
+    }
+    sendForm(form);
+    sendForm(formBottom);
 });
